9da9b7effc687f1d90caffcdd6af336981572ba7
Efim Poberezkin <efim@poberezkin.ru>
2017-12-12 00:59:00 +0400

    Added integration test for Airplane controller add method

--- src/test/java/com/epam/homework/springboot/SpringbootApplicationTests.java
+++ src/test/java/com/epam/homework/springboot/SpringbootApplicationTests.java
@@ -1,16 +1,56 @@
 package com.epam.homework.springboot;
 
+import com.epam.homework.springboot.domain.Airplane;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class SpringbootApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Autowired
+    private MockMvc mockMvc;
 
+    @Test
+    public void testAirplaneAdd() throws Exception {
+        String modelNumber = "Boeing747";
+        int capacity = 175;
+
+        Airplane airplane = new Airplane();
+        airplane.setModelNumber(modelNumber);
+        airplane.setCapacity(capacity);
+
+        mockMvc.perform(post("/airplanes")
+                .content(asJsonString(airplane))
+                .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.modelNumber", is(modelNumber)))
+                .andExpect(jsonPath("$.capacity", is(capacity)));
+    }
+
+
+    public static String asJsonString(final Object obj) {
+        try {
+            final ObjectMapper mapper = new ObjectMapper();
+            final String jsonContent = mapper.writeValueAsString(obj);
+            return jsonContent;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
